90f4a73ed5f032e4d2737f8dbad1ad6dac0dec30
Nick Wang <nickwang57@gmail.com>
2015-06-20 14:06:28 +0800

    set_operator

--- logfind/logfind.py
+++ logfind/logfind.py
@@ -1,10 +1,15 @@
 #build a logfind class skeleton
 import os
+import argparse
 
 class Logfind(object):
 
-  def __init__(self, operator = "and"):
+  def __init__(self):
+    #init the files to be searched
     self.filenames = os.path.join(os.getcwd(),'README.md')
+    self.operator = ""
+  #set the logic
+  def set_operator(self, operator):
     self.operator = operator
 
   def search(self, words):
@@ -15,11 +20,26 @@ class Logfind(object):
           return False
       return True
     else:
+      #todo 'or' logic
       return False
 
 def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-o', nargs='+')
+  parser.add_argument('words', nargs='+')
+
+  args = parser.parse_args()
+
   test = Logfind()
-  print(test.search(['logfind','project']))
+  if args.o is not None:
+    test.set_operator("or")
+  else:
+    test.set_operator("and")
+
+  #test = Logfind()
+  #print(test.search(['logfind','project']))
+  print("first arg ",args.words)
+  print("second arg",args.o)
 
 
 
--- tests/logfind_tests.py
+++ tests/logfind_tests.py
@@ -8,11 +8,17 @@ def setup():
 def teardown():
   print("TEAR DOWN!")
 
-def test_basic():
+def test_and_logic():
   test1 = Logfind()
+  test1.set_operator("and")
   assert_equal(test1.search(["logfind"]), True)
 
   test2 = Logfind()
+  test2.set_operator("and")
   assert_equal(test2.search(["logfind", "cool"]), False)
 
+def test_operator():
+  test = Logfind()
 
+  test.set_operator("and")
+  assert_equal(test.operator, "and")
